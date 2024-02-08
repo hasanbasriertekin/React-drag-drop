44c753c17fcbe3e0347e54a345806882bac1be08
hasan basri ertekin <hasanbasriertekin96@gmail.com>
2024-02-08 14:40:41 +0300

    drag and drop complated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import type { Quote as QuoteType } from '../types';
-import { nanoid } from 'nanoid';
-import { Button, Modal, Form } from 'react-bootstrap';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import type { Quote as QuoteType } from '../types'
+import { nanoid } from 'nanoid'
+import { Button, Modal, Form, InputGroup } from 'react-bootstrap'
+import './App.css'
 
 const initial = Array.from({ length: 10 }, (v, k) => k).map(k => {
   const custom: QuoteType = {
@@ -17,7 +16,7 @@ const initial = Array.from({ length: 10 }, (v, k) => k).map(k => {
 });
 
 const grid = 8;
-const reorder = (list: string, startIndex :number, endIndex:number): QuoteType[] => {
+const reorder = (list, startIndex, endIndex): QuoteType[] => {
   const result: QuoteType[] = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
@@ -64,7 +63,7 @@ function Quote({ quote, index, onEditClick }: { quote: QuoteType, index: number,
             <QuoteItem>
               {quote.content}
             </QuoteItem>
-            <Button variant="outline-light" className="edit-button" onClick={handleClick}>
+            <Button variant="outline-light fw-bold" className="edit-button" onClick={handleClick}>
               Düzenle
             </Button>
           </QuoteItemContainer>
@@ -90,7 +89,7 @@ function App() {
   useEffect(() => {
     // Sayfa açıldığında boş bir liste ile başla
     setState({ quotes: [] });
-  }, []); 
+  }, []);
 
   function onDragEnd(result: any) {
     if (!result.destination) {
@@ -139,39 +138,67 @@ function App() {
     handleEditClose();
   };
 
+  const handleDelete = () => {
+    const updatedQuotes = state.quotes.filter((quote) => quote.id !== editDialogTodoId);
+    setState({ quotes: updatedQuotes });
+    handleEditClose(); // Düzenleme modalini kapat
+  };
+
+
   return (
     <>
       <CenteredContainer>
-        <div className="app-container">
-          <input
-            className="input-add"
-            type="text"
-            value={newQuoteContent}
-            onChange={(e) => setNewQuoteContent(e.target.value)}
-            placeholder="Yeni şarkı ekle..."
-          />
-          <Button
-            variant="info"
-            size="lg"
-            onClick={() => {
-              if (newQuoteContent.trim() === "") {
-                return; // Boş alıntı eklenemez
-              }
+        <div className="app-container" >
+        <InputGroup className="mb-3" style={{ width: '700px' }}>
+            <Form.Control
+              placeholder="Listeye Ürün Ekle"
+              aria-label="Listeye Ürün Ekle"
+              aria-describedby="basic-addon2"
+              value={newQuoteContent}
+              onChange={(e) => setNewQuoteContent(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  if (newQuoteContent.trim() === "") {
+                    return; // Boş alıntı eklenemez
+                  }
 
-              const newQuote: QuoteType = {
-                id: `id-${nanoid()}`,
-                content: newQuoteContent
-              };
+                  const newQuote: QuoteType = {
+                    id: `id-${nanoid()}`,
+                    content: newQuoteContent
+                  };
 
-              setState((prevState) => ({
-                quotes: [...prevState.quotes, newQuote]
-              }));
+                  setState((prevState) => ({
+                    quotes: [...prevState.quotes, newQuote]
+                  }));
 
-              setNewQuoteContent(""); // Input'u temizle
-            }}
-          >
-            Ekle
-          </Button>
+                  setNewQuoteContent(""); // Input'u temizle
+                }
+              }}
+            />
+            <Button
+              variant="success"
+              size="lg"
+              onClick={() => {
+                if (newQuoteContent.trim() === "") {
+                  return; // Boş alıntı eklenemez
+                }
+
+                const newQuote: QuoteType = {
+                  id: `id-${nanoid()}`,
+                  content: newQuoteContent
+                };
+
+                setState((prevState) => ({
+                  quotes: [...prevState.quotes, newQuote]
+                }));
+
+                setNewQuoteContent(""); // Input'u temizle
+              }}
+              id="button-addon2"
+            >
+              Ekle
+            </Button>
+          </InputGroup>
 
           <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="list">
@@ -199,6 +226,9 @@ function App() {
           <Modal.Footer>
             <Button variant="secondary" onClick={handleEditClose}>
               İptal
+            </Button>
+            <Button variant="danger" onClick={handleDelete}>
+              Sil
             </Button>
             <Button variant="primary" onClick={handleEditSave}>
               Kaydet
